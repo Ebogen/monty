194285b31db7a3cb8e4252e4812db1fffcd27674
Ebogen <ebogensamuel@gmail.com>
2022-07-14 21:30:20 -0700

    doubly_funct

---
+++ doubly_funct.c
@@ -0,0 +1,92 @@
+#include "monty.h"
+
+/**
+ *add_dnodeint_end - this is a function that add a note at the
+ *                   end of the doubly link list
+ *@head: this represents the first position of linked list
+ *@n: this represents the data to store
+ *Return: we get a doubly linked list
+ */
+stack_t *add_dnodeint_end(stack_t **head, const int n)
+{
+	stack_t *temp, *aux;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_globalv();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Here we need to be careful with the first time*/
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	aux = *head;
+	while (aux->next)
+		aux = aux->next;
+	temp->next = aux->next;
+	temp->prev = aux;
+	aux->next = temp;
+	return (aux->next);
+}
+
+/**
+ *add_dnodeint - This is a function that adds a node at the
+ *               begining of the doubly link list
+ *@head: this represents the first position of linked list
+ *@n: this represents the data we want to store
+ *Return: this gives a doubly linked list
+ */
+stack_t *add_dnodeint(stack_t **head, const int n)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_globalv();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Here we need to be careful*/
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	(*head)->prev = temp;
+	temp->next = (*head);
+	temp->prev = NULL;
+	*head = temp;
+	return (*head);
+}
+
+/**
+ * free_dlistint - this is a function that frees the doubly
+ *                 linked list
+ * @head: head of the list
+ * Return: no return
+ */
+void free_dlistint(stack_t *head)
+{
+	stack_t *tmp;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
